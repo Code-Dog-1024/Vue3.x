ea8cda61155c137c9df6d66fcfbc8afde0d5275a
Code-Dog-1024 <lize981001@163.com>
2021-06-28 20:52:45 +0800

    fix(utils): 新增防抖、节流工具函数

---
+++ src/@types/basic.d.ts
@@ -0,0 +1,5 @@
+export interface Dictionary<T = any> {
+  [k: string]: T;
+}
+
+export type Foo = () => void;
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,10 +1,17 @@
+import { Dictionary, Foo } from "@/@types/basic.d";
+
 /**
  * 数字添加分隔符并保留两位小数
  *
- * @param {number | string} value
- * @returns {string} `1,000.00`
+ * @param {number | string} value 数字或字符串格式数字
+ * @template
+ * ```js
+ * formatNumber(1000) => 1,000.00
+ * ```
+ * @returns `string`
  */
 export const formatNumber = (value: number | string): string => {
+  value = parseFloat(String(value));
   const arr = String(value).split(".");
   const intPartArr = arr[0].split("").reverse();
   const floatPartArr = arr[1] ? arr[1].split("") : [];
@@ -35,4 +42,57 @@ export const formatNumber = (value: number | string): string => {
   }
 
   return `${intPart}.${floatPart}`;
+};
+
+interface DebounceReturn {
+  (...arg: []): number;
+}
+
+/**
+ * 节流
+ *
+ * @param {number} timeout 定时器时延
+ * @param {Foo} callback 定时器回调
+ * @returns {DebounceReturn}
+ */
+export const debounce = (timeout: number, callback: Foo): DebounceReturn => {
+  let timer = 0;
+  return (...arg: []) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(callback, timeout, arg);
+    return timer;
+  };
+};
+
+interface ThrottleReturn {
+  (...arg: []): Foo;
+}
+
+/**
+ * 防抖
+ *
+ * @param {number} timeout 定时器时延
+ * @param {Foo} callback 定时器回调
+ * @returns {ThrottleReturn} 用于清除定时器的函数
+ */
+export const throttle = (timeout: number, callback: Foo): ThrottleReturn => {
+  let isFirst = true;
+  let timer = 0;
+  const clearFun: Foo = () => {
+    clearTimeout(timer);
+    timer = 0;
+  };
+  return (...arg: []) => {
+    if (isFirst) {
+      isFirst = false;
+      callback(...arg);
+      return clearFun;
+    }
+    if (timer) return clearFun;
+    timer = setTimeout(() => {
+      timer = 0;
+      callback(...arg);
+    }, timeout);
+    return clearFun;
+  };
 };
